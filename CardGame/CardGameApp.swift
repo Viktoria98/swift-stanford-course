948a1b444fc56275786746c6eb25d499b50a15d7
Vika <krivosheevaviktoria98@gmail.com>
2021-01-12 00:55:50 +0200

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Viktoria Kryvosheeva on 12.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
